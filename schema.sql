2f14d3f0b4e46f834a1bb13e3ee6373adb361274
Ma <asasf@sfsfsdf.com>
2023-06-10 01:54:22 +0300

    Updated table users and added table profiles in schema.sql. Fixed a broken sql command in users.py in the method register() accordingly.

--- schema.sql
+++ schema.sql
@@ -1,8 +1,18 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username TEXT UNIQUE,
-    password TEXT
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    admin boolean DEFAULT False,
+    created TIMESTAMP
 );
+
+CREATE TABLE profiles (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    profile_name TEXT UNIQUE NOT NULL,
+    created TIMESTAMP
+);
+
 CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
     content TEXT,
